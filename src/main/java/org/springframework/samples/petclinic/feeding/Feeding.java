b256eeeebcb1e1dc62dbbf2d16b499e36e239e12
jvegax <toledo.utrera@hotmail.com>
2022-01-14 10:55:04 +0100

    Test 1

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,10 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -10,8 +14,22 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Feeding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     Integer id;
+            
+    @NotNull
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Column(name = "start_date")
     LocalDate startDate;
+
+    @NotNull
+    @PositiveOrZero
+    @Column(name = "weeks_duration")
     double weeksDuration;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
     Pet pet;   
 }
